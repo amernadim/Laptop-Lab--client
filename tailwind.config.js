1262800bfa1bf9f7f68316269f7f4230f93bcb35
Your Name <you@example.com>
2022-11-24 00:23:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#6ECCAF",
+        
+"secondary": "#497174",
+        
+"accent": "#344D67",
+        
+"neutral": "#171424",
+        
+"base-100": "#D6E4E5",
+        
+"info": "#3F85CF",
+        
+"success": "#155B53",
+        
+"warning": "#F3C74F",
+        
+"error": "#E14023",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
