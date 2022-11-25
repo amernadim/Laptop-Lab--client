763882094efe1d21f003d64d140f921ec12703bf
Your Name <you@example.com>
2022-11-26 04:58:18 +0600

    add my product route

--- src/component/Dashboard/MyProduct.jsx
+++ src/component/Dashboard/MyProduct.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthProvider';
+import Spinner from '../Shared/Spinner';
 
 const MyProduct = () => {
+  const {user} = useContext(AuthContext);
+
+  const { data = [] ,isLoading } = useQuery({
+    queryKey: ["product"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/product/${user?.email}`);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  console.log(data);
+
+  if(isLoading) {
+    return <Spinner/>
+  }
   return (
       <div className='w-11/12 mx-auto mb-4  overflow-x-auto'>
      <div><h2 className="text-xl font-semibold text-center">My Products</h2></div>
@@ -15,31 +34,37 @@ const MyProduct = () => {
         </tr>
       </thead>
       <tbody>
-        {/* <!-- row 1 --> */}
-        <tr>
+        {
+          data.map((product,i) => <tr key={i}>
           <td>
             <div className="flex items-center space-x-3">
               <div className="avatar">
                 <div className="mask mask-squircle w-12 h-12">
-                  <img src="/tailwind-css-component-profile-2@56w.png" alt="#" />
+                  <img src={product?.productImg} alt="#" />
                 </div>
               </div>
               <div>
-                <h3 className="font-bold">Hart Hagerty</h3>
+                <h3 className="font-bold">{product?.productName}</h3>
               </div>
             </div>
           </td>
           <td>
-            <p>1000tk</p>
+            <p>{product?.resalePrice}</p>
           </td>
-          <td>
+          <td> {
+            product?.available === "true" ? 
             <button className='btn btn-ghost'>Available</button>
-            <button className='btn btn-ghost ml-2'>Sold</button>
+             :
+             <button className='btn btn-ghost ml-2'>Sold</button>            
+            }
           </td>
           <th>
             <button className="btn btn-sm">Advertise</button>
           </th>
-        </tr>
+        </tr>  )
+
+        }
+       
      
       </tbody>
     </table>
